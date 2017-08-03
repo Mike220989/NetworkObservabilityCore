61aeee24eb0f0d984971974d073c7f5dc4fe19b9
MxtfV <mustafa.logan@outlook.com.au>
2017-08-03 20:15:14 +1000

    Added the Path and PathList classes

---
+++ NetworkObservabilityCore/Path.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkObservabilityCore
+{
+    class Path
+    {
+        public double Cost { get; set; }
+        private List<Node> path;
+
+        public Path()
+        {
+            path = new List<Node>();
+         
+        }
+
+        public void add(Node node)
+        {
+            path.Add(node);
+            
+        }
+
+        public Node Source => path[0];
+
+        public Node Destination => path[path.Count - 1];
+
+        public double GetPathCost()
+        {
+            //double cost = 0;
+            //foreach(var tempPath in path)
+            //{
+            //    //
+            //}
+                throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetworkObservabilityCore/PathList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkObservabilityCore
+{
+    class PathList
+    {
+        private List<Path> paths;
+        private Dictionary<KeyValuePair<Node, Node>, List<Path>> pathGroups;
+        private bool max;
+
+        public PathList(bool max)
+        {
+            this.max = max;
+            paths = new List<Path>();
+            pathGroups = new Dictionary<KeyValuePair<Node, Node>, List<Path>>();
+        }
+
+
+        private void GroupPath()
+        {
+            foreach (Path path in paths)
+            {
+                pathGroups[new KeyValuePair<Node, Node>(path.Source, path.Destination)] = new List<Path>();
+            }
+
+            foreach (Path path in paths)
+            {
+                var list = pathGroups[new KeyValuePair<Node, Node>(path.Source, path.Destination)];
+                list.Add(path);
+            }
+        }
+
+        private void FilterPath()
+        {
+            Dictionary<KeyValuePair<Node, Node>, List<Path>> newPathGroups = new Dictionary<KeyValuePair<Node, Node>, List<Path>>();
+            foreach (var paths in pathGroups)
+            {
+                List<Path> mins = new List<Path>();
+            
+                foreach (Path path in paths.Value)
+                {
+                    if (mins.Count == 0 || path.Cost <= mins[0].Cost )
+                    {
+                        if (path.Cost < mins[0].Cost)
+                            mins.Clear();
+                        mins.Add(path);
+                    }
+                }
+                newPathGroups[paths.Key] = mins;
+            }
+            pathGroups = newPathGroups;
+        }
+
+        public Dictionary<KeyValuePair<Node, Node>, List<Path>> GetAllThePaths()
+        {
+            this.GroupPath();
+            this.FilterPath();
+            return pathGroups;
+        }
+    }
+}
